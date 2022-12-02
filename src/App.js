1b933fa05f364cf425fe08df88ce66277c59a718
Jay <jjjjuhyuneee@naver.com>
2022-12-02 18:15:40 +0900

    1202

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
 import './css/common.scss'
 
 import Header from './components/Header'
+import Main from './components/Main'
+import New from './components/New'
+import Woman from './components/Woman'
+import Man from './components/Man'
+import Collection from './components/Collection'
+import Magazine from './components/Magazine'
+import Brand from './components/Brand'
 import Footer from './components/Footer'
 
 
@@ -10,6 +18,17 @@ const App = () => {
     <>
       <div className='Wrapper'>
         <Header />
+
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/new' element={<New />} />
+          <Route path='/woman' element={<Woman />} />
+          <Route path='/man' element={<Man />} />
+          <Route path='/collection' element={<Collection />} />
+          <Route path='/magazine' element={<Magazine />} />
+          <Route path='/brand' element={<Brand />} />
+        </Routes>
+
         <Footer />
       </div>
     </>
