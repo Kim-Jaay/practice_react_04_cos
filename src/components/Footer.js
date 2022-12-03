bb152b78d6eec2500a4d4ae7e69391b58dbdc37a
Jay <jjjjuhyuneee@naver.com>
2022-12-03 20:11:53 +0900

    1203

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className='Footer'>
+            <ul>
+                <li>고객서비스</li>
+                <li>매장찾기</li>
+            </ul>
+        </footer>
     )
 }
 
