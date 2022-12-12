29482a4a9614d6605c132df2f8e3d1a99d4d18e9
Kim-Jaay <jjjjuhyuneee@naver.com>
2022-12-12 16:19:52 +0900

    1212

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,57 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import '../css/Footer.scss'
 const Footer = () => {
     return (
         <footer className='Footer'>
-            <ul>
-                <li>고객서비스</li>
-                <li>매장찾기</li>
-            </ul>
+
+            <div className="top">
+                <div className="inner">
+                    <div className="top_con">
+                        <ul>
+                            <li><Link>고객서비스</Link></li>
+                            <li><Link>매장찾기</Link></li>
+                            <li><Link>배송정보</Link></li>
+                            <li><Link>반품 및 환불</Link></li>
+                            <li><Link>결제정보</Link></li>
+                        </ul>
+                        <ul>
+                            <li><Link>지속가능성</Link></li>
+                            <li><Link>협력업체</Link></li>
+                            <li><Link>다양성 & 포용성</Link></li>
+                            <li><Link>COS By You</Link></li>
+                            <li><Link>제품관리</Link></li>
+                            <li><Link>채용정보</Link></li>
+                            <li><Link>프레스 문의</Link></li>
+                            <li><Link>고객문의</Link></li>
+                        </ul>
+                        <ul>
+                            <li><a href="">Kakaotalk</a></li>
+                            <li><a href="">Instagram</a></li>
+                            <li><a href="">Facebook</a></li>
+                            <li><a href="">Pinterest</a></li>
+                            <li><a href="">Spotify</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="bot">
+                <div className="inner">
+                    <div className="bot_con">
+                        <h1>
+                            <Link to='/'>
+                                <img src={process.env.PUBLIC_URL + '/assets/img/cos_logo.svg'} alt="" />
+                            </Link>
+                        </h1>
+                        <ul>
+                            <li><Link>gg</Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
         </footer>
     )
 }
--- src/components/MainCon.js
+++ src/components/MainCon.js
@@ -7,7 +7,7 @@ const MainCon = () => {
 
         <div className="inner">
             <ul className='main_con'>
-                <li> <Link><img src={process.env.PUBLIC_URL + '/assets/img/MainCon01.png'} alt="" /></Link></li>
+                <li><Link><img src={process.env.PUBLIC_URL + '/assets/img/MainCon01.png'} alt="" /></Link></li>
                 <li><Link><img src={process.env.PUBLIC_URL + '/assets/img/MainCon02.png'} alt="" /></Link></li>
                 <li><Link><img src={process.env.PUBLIC_URL + '/assets/img/MainCon03.png'} alt="" /></Link></li>
                 <li><Link><img src={process.env.PUBLIC_URL + '/assets/img/MainCon04.png'} alt="" /></Link></li>
