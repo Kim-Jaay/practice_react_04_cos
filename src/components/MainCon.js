59f59bade32e138e0e206c1439f21b335b21136c
Kim-Jaay <jjjjuhyuneee@naver.com>
2022-12-12 15:30:41 +0900

    1212

--- src/components/MainCon.js
+++ src/components/MainCon.js
@@ -12,9 +12,11 @@ const MainCon = () => {
                 <li><Link><img src={process.env.PUBLIC_URL + '/assets/img/MainCon03.png'} alt="" /></Link></li>
                 <li><Link><img src={process.env.PUBLIC_URL + '/assets/img/MainCon04.png'} alt="" /></Link></li>
                 <li>
-                    <p>SUSTAINABILITY AT COS</p>
-                    <h2>Beyond Neutral</h2>
-                    <Link>Brand Stroy <i className='xi-angle-right-thin'></i></Link>
+                    <div className="des">
+                        <p>SUSTAINABILITY AT COS</p>
+                        <h2>Beyond Neutral</h2>
+                        <Link>Brand Stroy <i className='xi-angle-right-thin'></i></Link>
+                    </div>
                 </li>
                 <li><Link><img src={process.env.PUBLIC_URL + '/assets/img/MainCon06.png'} alt="" /></Link></li>
                 <li><Link><img src={process.env.PUBLIC_URL + '/assets/img/MainCon07.png'} alt="" /></Link></li>
