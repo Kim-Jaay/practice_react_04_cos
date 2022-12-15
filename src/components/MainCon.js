9665a7d9d2baaa3c46b204408efa26664e09bb3b
Jay <jjjjuhyuneee@naver.com>
2022-12-15 12:58:18 +0900

    1215

--- src/components/MainCon.js
+++ src/components/MainCon.js
@@ -7,21 +7,21 @@ const MainCon = () => {
 
         <div className="inner">
             <ul className='main_con'>
-                <li><Link><img src={process.env.PUBLIC_URL + '/assets/img/MainCon01.png'} alt="" /></Link></li>
-                <li><Link><img src={process.env.PUBLIC_URL + '/assets/img/MainCon02.png'} alt="" /></Link></li>
-                <li><Link><img src={process.env.PUBLIC_URL + '/assets/img/MainCon03.png'} alt="" /></Link></li>
-                <li><Link><img src={process.env.PUBLIC_URL + '/assets/img/MainCon04.png'} alt="" /></Link></li>
+                <li> <Link to='/new'><img src={process.env.PUBLIC_URL + '/assets/img/MainCon01.png'} alt="" /></Link></li>
+                <li> <Link to='/new'><img src={process.env.PUBLIC_URL + '/assets/img/MainCon02.png'} alt="" /></Link></li>
+                <li> <Link to='/new'><img src={process.env.PUBLIC_URL + '/assets/img/MainCon03.png'} alt="" /></Link></li>
+                <li> <Link to='/new'><img src={process.env.PUBLIC_URL + '/assets/img/MainCon04.png'} alt="" /></Link></li>
                 <li>
                     <div className="des">
                         <p>SUSTAINABILITY AT COS</p>
                         <h2>Beyond Neutral</h2>
-                        <Link>Brand Stroy <i className='xi-angle-right-thin'></i></Link>
+                        <Link to='/story'>Brand Stroy <i className='xi-angle-right-thin'></i></Link>
                     </div>
                 </li>
-                <li><Link><img src={process.env.PUBLIC_URL + '/assets/img/MainCon06.png'} alt="" /></Link></li>
-                <li><Link><img src={process.env.PUBLIC_URL + '/assets/img/MainCon07.png'} alt="" /></Link></li>
-                <li><Link><img src={process.env.PUBLIC_URL + '/assets/img/MainCon08.png'} alt="" /></Link></li>
-                <li><Link><img src={process.env.PUBLIC_URL + '/assets/img/MainCon09.png'} alt="" /></Link></li>
+                <li> <Link to='/new'><img src={process.env.PUBLIC_URL + '/assets/img/MainCon06.png'} alt="" /></Link></li>
+                <li> <Link to='/new'><img src={process.env.PUBLIC_URL + '/assets/img/MainCon07.png'} alt="" /></Link></li>
+                <li> <Link to='/new'><img src={process.env.PUBLIC_URL + '/assets/img/MainCon08.png'} alt="" /></Link></li>
+                <li> <Link to='/new'><img src={process.env.PUBLIC_URL + '/assets/img/MainCon09.png'} alt="" /></Link></li>
             </ul>
         </div>
     )
