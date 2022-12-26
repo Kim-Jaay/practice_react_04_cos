9ece9e2509304dc45863d294fe79f1c44c3a37ac
Kim-Jaay <jjjjuhyuneee@naver.com>
2022-12-26 15:18:39 +0900

    1226

--- src/data/common.js
+++ src/data/common.js
@@ -4,28 +4,22 @@ const NEW = [
 ]
 
 const WOMAN = [
-    { id: 1, menu: '세일', link: '/sale' },
-    { id: 2, menu: '트렌딩 나우', link: '/trend' },
-    { id: 3, menu: '아우터웨어', link: '/outer' },
-    { id: 4, menu: '니트웨어', link: '/knit' },
+    { id: 1, menu: '트렌딩 나우', link: '/trend' },
+    { id: 2, menu: '니트웨어', link: '/knit' },
 ]
 
 const MAN = [
-    { id: 1, menu: '세일', link: '/sale' },
-    { id: 2, menu: '트렌딩 나우', link: '/trend' },
-    { id: 3, menu: '아우터웨어', link: '/outer' },
-    { id: 4, menu: '니트웨어', link: '/knit' },
+    { id: 1, menu: '트렌딩 나우', link: '/trend' },
+    { id: 2, menu: '니트웨어', link: '/knit' },
 ]
 
 const GIFT = [
-    { id: 1, menu: '여성 기프트컬렉션', link: '/woman' },
-    { id: 2, menu: '남성 기프트컬렉션', link: '/man' },
+    { id: 1, menu: '기프트컬렉션', link: '/woman' },
 ]
 
 const MAGAZINE = [
-    { id: 1, menu: '최신', link: '/new' },
-    { id: 2, menu: '스토리', link: '/story' },
-    { id: 3, menu: '브랜드', link: '/brand' },
+    { id: 1, menu: '스토리', link: '/story' },
+    { id: 2, menu: '브랜드', link: '/brand' },
 ]
 
 export { NEW, WOMAN, MAN, GIFT, MAGAZINE }
